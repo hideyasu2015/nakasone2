51ade0348a4cb6a20a18890470c9cac350504738
Miwako Nakasone <anothersky79@gmail.com>
2019-07-01 10:05:12 +0900

    timeLine

--- tpl_090_rwd/page-aboutus.php
+++ tpl_090_rwd/page-aboutus.php
@@ -1,24 +1,66 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="contentsWrap">
-        <div class="mainContents">
-            <?php 
-              if(have_posts() ):
-              while(have_posts() ) : the_post();
-            ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class('page'); ?>>
-                <h2 class="main_menu"><?php the_title(); ?></h2>
-                <section class="content">
-                    <?php the_content(); ?>
-                </section>
-            </article> <!-- /.page -->
-            <?php  
-           endwhile;
-          endif;
-          ?>
-        </div> <!-- /.mainContents -->
-    </div> <!-- /.contentsWrap -->
+    <div class="page-header">
+        <h1 id="timeline">ABOUT US</h1>
+    </div>
+    <ul class="timeline">
+        <li>
+            <div class="timeline-badge danger"><i class="glyphicon glyphicon-check"></i></div>
+            <div class="timeline-panel">
+                <div class="timeline-heading">
+                    <h4 class="timeline-title">Thai Noodles</h4>
+                    <!-- <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> 11 hours ago via
+                            Twitter</small></p> -->
+                </div>
+                <div class="timeline-body">
+                    <p>Tradition since 1889 It is a shop of rice noodles with a reputation as very delicious.
+                        <p>
+                            <div class="item">
+                                <img src="<?php bloginfo('template_directory'); ?>/images/gm.png" class="d-block w-100"
+                                    alt="...">
+                            </div>
+                </div>
+            </div>
+        </li>
+        <li class="timeline-inverted">
+            <div class="timeline-badge info"><i class="glyphicon glyphicon-credit-card"></i></div>
+            <div class="timeline-panel">
+                <div class="timeline-heading">
+                    <h4 class="timeline-title">It is open from </h4>
+                </div>
+                <div class="timeline-body">
+                    <h2>11:00 to 16:00.</h2>
+                </div>
+
+        </li>
+
+
+        <li>
+            <div class="timeline-badge"><i class="glyphicon glyphicon-floppy-disk"></i></div>
+            <div class="timeline-panel">
+                <div class="timeline-heading">
+                    <h4 class="timeline-title">Cooking</h4>
+                </div>
+                <div class="timeline-body">
+                    <p>You surely will be satisfied. The second owner himself is making it with all my heart. It is
+                        cheap, tasty and popular. The location is difficult to understand. That's why it's OK. I am
+                        making a soup from the morning. In the afternoon, delicious, rice noodles can be eaten. </p>
+                </div>
+            </div>
+        </li>
+
+        <li class="timeline-inverted">
+            <div class="timeline-badge success"><i class="glyphicon glyphicon-thumbs-up"></i></div>
+            <div class="timeline-panel">
+                <div class="timeline-heading">
+                    <div class="item">
+                        <img src="<?php bloginfo('template_directory'); ?>/images/obachan.png" class="d-block w-100"
+                            alt="...">
+                    </div>
+                </div>
+        </li>
+    </ul>
 </div>
 
 
