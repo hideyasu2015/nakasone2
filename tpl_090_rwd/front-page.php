3c893b82721fb86728b11cc8564aae3dc0cabbc8
Miwako Nakasone <anothersky79@gmail.com>
2019-09-23 19:48:11 +0900

    9/23css

--- tpl_090_rwd/front-page.php
+++ tpl_090_rwd/front-page.php
@@ -92,29 +92,30 @@
         </div>
         <div class="col-md-6">
             <div id="map"></div>
-        </div>
 
-        <script>
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 18,
-                center: {
-                    lat: 18.753481,
-                    lng: 99.020067
-                }
-            });
-            var marker = new google.maps.Marker({
-                map: map,
-                position: {
-                    lat: 18.753481,
-                    lng: 99.020067
-                }
-            });
-        }
-        </script>
-        <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWsZ4gHwTMP1gq7b-i94jKkih_lnlptGc&callback=initMap">
-        </script>
+
+            <script>
+            function initMap() {
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 18,
+                    center: {
+                        lat: 18.753481,
+                        lng: 99.020067
+                    }
+                });
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: {
+                        lat: 18.753481,
+                        lng: 99.020067
+                    }
+                });
+            }
+            </script>
+            <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWsZ4gHwTMP1gq7b-i94jKkih_lnlptGc&callback=initMap">
+            </script>
+        </div>
     </div>
 </div>
 
--- tpl_090_rwd/header.php
+++ tpl_090_rwd/header.php
@@ -53,23 +53,24 @@ wp_head(); ?>
             </div>
             <?php endif; ?>
 
-            <!-- globalNavi -->
-            <div class="container">
-                <nav class="globalNavi navbar-expand-sm navbar-light">
-                    <button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        </div>
 
-                    <div id="menu" class="collapse navbar-collapse">
-                        <?php 
+        <!-- globalNavi -->
+        <nav class="navbar  navbar-expand-sm navbar-dark  light-green">
+            <!-- <a class="navbar-brand" href="http://parnee-noodle-shop.local/">HOME</a> -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse justify-content-center navbar-collapse " id="navbarNav">
+                <?php 
                         $args=array(
                             'menu'=>'global-navigation',
                             'container'=>false,
                         );
                         wp_nav_menu($args);
                         ?>
-                    </div>
-                </nav>
             </div>
+        </nav>
 
     </header>
